0e4547c082d95c7074625d170f8383c4ed58a664
Redbeard29 <ben@benshermanmusic.com>
2022-02-03 13:20:59 -0500

    solved remove even integers

---
+++ DSForInterviewsCSharp/DSForInterviewsCSharp/RemoveEvenIntegers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DSForInterviewsCSharp
+{
+    class RemoveEvenIntegers
+    {
+        static int[] removeEven(int[] initialArr)
+        {
+            int y = 0;
+            for(int x = 0; x < initialArr.Length; x++)
+            {
+                if(initialArr[x] % 2 != 0)
+                {
+                    initialArr[y] = initialArr[x];
+                    y++;
+                }
+            }
+            int[] newArr = new int[y];
+            for(int x = 0; x < y; x++) {
+                newArr[x] = initialArr[x];
+            }
+            return newArr;
+        }
+
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+            for(int x = 0; x < arr.Length; x++) {
+                arr[x] = x;
+            }
+            int[] newArr = removeEven(arr);
+            Array.ForEach(newArr, Console.Write);
+        }
+
+    }
+}
